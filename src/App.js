5a9fed66059b9a114417bb1abd754a85c67a7838
Trey Hoover <treyhoover@gmail.com>
2017-06-30 19:31:24 -0400

    Fix mobile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import bowser from "bowser";
 import video from "./video.mp4";
 import './App.css';
 
+import volumeOff from "./volumeOff.svg";
+import volumeOn from "./volumeOn.svg";
+
 const RELEASE_DATE = moment("2017-07-30T23:30:00-04:00");
+const IS_DESKTOP = !bowser.mobile && !bowser.tablet;
 
 class App extends Component {
+  state = {
+    muted: !IS_DESKTOP, // mobile and tablet devices need to start muted in order to autoplay
+  };
+
   componentDidMount() {
     this.countdown = setInterval(() => {
       this.forceUpdate();
@@ -20,27 +29,53 @@ class App extends Component {
     return moment.duration(RELEASE_DATE.diff(moment()));
   }
 
+  toggleMute = () => {
+    this.setState({
+      muted: !this.state.muted,
+    });
+
+    this.videoNode.muted = !this.videoNode.muted;
+  };
+
+  onLinkClick = (e) => {
+    e.stopPropagation();
+  };
+
+  bindRef = videoNode => {
+    this.videoNode = videoNode;
+  };
+
   render() {
+    const { muted } = this.state;
     const remaining = this.duration;
     const isReleased = remaining.asSeconds() <= 0;
+    const volumeIcon = muted ? volumeOff : volumeOn;
 
     return (
-      <div className="App">
-        <video autoPlay loop id="video-background" playsInline>
+      <div className="App" onClick={this.toggleMute}>
+        <video ref={this.bindRef} autoPlay muted={this.state.muted} loop id="video-background"
+               playsInline>
           <source src={video} type="video/mp4" />
         </video>
 
-        <a target="_tab" href="https://twitter.com/treytherobot" className="twitter">@treytherobot</a>
+        <div className="AppHeader">
+          <img className="volume" src={volumeIcon} alt="Volume" />
 
-        <div className="countDown">
-          {isReleased ? <h1>
-            IT'S OUT!
-          </h1> : <h1>
-            {remaining.days()} {'days '}
-            {remaining.hours()} {'hours '}
-            {remaining.minutes()} {'minutes '}
-            {remaining.seconds()} {'seconds '}
-          </h1>}
+          <a onClick={this.onLinkClick} target="_tab" href="https://twitter.com/treytherobot"
+             className="twitter">@treytherobot</a>
+        </div>
+
+        <div className="AppContent">
+          <div className="countDown">
+            {isReleased ? <h1>
+              IT'S OUT!
+            </h1> : <h1>
+              {remaining.days()} {'days '}
+              {remaining.hours()} {'hours '}
+              {remaining.minutes()} {'minutes '}
+              {remaining.seconds()} {'seconds '}
+            </h1>}
+          </div>
         </div>
       </div>
     );
