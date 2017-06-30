4245d9acc962791fabab1a100c598a820be19a04
Trey Hoover <treyhoover@gmail.com>
2017-06-29 18:31:40 -1100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import moment from "moment";
+import video from "./video.mp4";
+import './App.css';
+
+const RELEASE_DATE = moment("2017-07-30T23:30:00-04:00");
+
+class App extends Component {
+  componentDidMount() {
+    this.countdown = setInterval(() => {
+      this.forceUpdate();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.countdown);
+  }
+
+  get duration() {
+    return moment.duration(RELEASE_DATE.diff(moment()));
+  }
+
+  render() {
+    const remaining = this.duration;
+    const isReleased = remaining.asSeconds() <= 0;
+
+    return (
+      <div className="App">
+        <video autoPlay loop id="video-background" playsInline>
+          <source src={video} type="video/mp4" />
+        </video>
+
+        <div className="countDown">
+          {isReleased ? <h1>
+            IT'S OUT!
+          </h1> : <h1>
+            {remaining.days()} {'days '}
+            {remaining.hours()} {'hours '}
+            {remaining.minutes()} {'minutes '}
+            {remaining.seconds()} {'seconds '}
+          </h1>}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
